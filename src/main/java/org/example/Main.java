222a467f901da7ff1dc3304f62f7e93d4c98ad07
Juan Pablo <jguevara@cesde.net>
2024-03-14 11:39:32 -0500

    practica

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,47 @@
+package org.example;
+
+import org.example.paqueteDos.Animal;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // las funciones se usan llamandolas
+
+        //int resultado=sumar(5,10);
+        //System.out.println("La suma es:"+resultado);
+
+
+
+        //public static int sumar(int numero1, int numero2){
+        // return numero1+numero2;}
+
+
+        //variable simple en java
+        Integer edad=28;
+
+        //variable tipo objeto
+        Animal objeto=new Animal();
+        Animal objeto2=new Animal(5,"leonsio");
+
+
+
+        Veterinario objeto3=new Veterinario();
+        Veterinario objeto4=new Veterinario(10,"Antonio","1042515256");
+
+        //Accediendo a los atributos de una clase desde un objeto
+        //pongo el nombre del objeto.nombre del atributo al que quiero acceder
+
+        objeto4.
+
+
+
+
+
+    }
+
+
+
+
+
+
+}
